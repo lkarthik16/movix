f5c9f3524dbb5256c9db4ceabe91e74ef2e95b9b
lkarthik16 <152290686+lkarthik16@users.noreply.github.com>
2025-03-16 17:33:22 +0530

    Updated

--- movix/settings.py
+++ movix/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.getenv("DEBUG", "False") == "True"  # Render will use this
+DEBUG = os.getenv("DEBUG", "False") == "False"  # Render will use this
 
 ALLOWED_HOSTS = [
     'movix-q09j.onrender.com', # Allows all Render subdomains
