977947058e4caa25f4d0002e0b171187182c27f9
lkarthik16 <152290686+lkarthik16@users.noreply.github.com>
2025-03-16 15:15:50 +0530

    Updated

--- movix/settings.py
+++ movix/settings.py
@@ -19,17 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.getenv("DEBUG", "False") == "True"  # Render will use this
 
 ALLOWED_HOSTS = [
-    'movix-q09j.onrender.com',
+    'movix-q09j.onrender.com', # Allows all Render subdomains
     'localhost',  # For local development
     '127.0.0.1',  # For local development
 ]
 
 # Application definition
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movixapp',  # Your app
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # For serving static files in production
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -67,7 +67,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'movix.wsgi.application'
 
 # Database
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
